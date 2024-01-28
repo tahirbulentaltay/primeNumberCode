e0c2cf6e42c7118c109610c875518fda5dffe6b7
Tahir Bülent Altay <altaytb1976@yahoo.com>
2024-01-28 16:20:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let statement = document.querySelector("#statement");
+let prime = document.querySelector("#prime");
+let notPrime = document.querySelector("#notPrime");
+let primeNumbers = document.querySelector("#primeNumbers");
+let nonPrimeNumbers = document.querySelector("#nonPrimeNumbers");
+let sumPrimeNumber = 0;
+let sumNotPrimeNumber = 0;
+let x = 50;
+
+for (let i = 2; i <= x; i++) {
+    let result = true;
+
+    for (let j = 2; j <= Math.floor(i / 2); j++) {
+
+        if (i % j == 0) {
+            document.write(`<b>${i}</b> not prime number. `)
+            console.log(i + " not prime number.");
+            result = false;
+            sumNotPrimeNumber = sumNotPrimeNumber + i;
+            console.log("Sum of the non prime numbers: " + sumNotPrimeNumber);
+            break;
+        }
+    }
+    if (result) {
+        document.write(`<b>${i}</b> prime number. `);
+        console.log(i + " prime number.");
+        sumPrimeNumber = sumPrimeNumber + i
+        console.log("Sum of the prime numbers: " + sumPrimeNumber);
+    }
+}
+statement.innerHTML = `The prime and non prime numbers between <b>2</b> and <b>${x}</b> and sum of them.`
+prime.innerHTML = `Sum of the prime numbers: <b>${sumPrimeNumber}</b>`;
+notPrime.innerHTML = `Sum of the non prime numbers: <b>${sumNotPrimeNumber}</b>`;
